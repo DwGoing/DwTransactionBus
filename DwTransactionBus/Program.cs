ebc9936c1568316814ac53b99bfad99a4f8027d7
DwGoing <jianghy1209@163.com>
2020-07-31 16:38:56 +0800

    测试

--- DwTransactionBus/Program.cs
+++ DwTransactionBus/Program.cs
@@ -19,6 +19,7 @@ namespace DwTransactionBus
                 host.InitService(provider => provider.InitWebAPIServiceAsync<Startup>());
                 host.RegisterRabbitMQService();
                 host.Run();
+                // test
             }
             catch (Exception ex)
             {
