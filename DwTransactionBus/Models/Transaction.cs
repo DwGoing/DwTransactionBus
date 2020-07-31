ce5c2da7fabebe2c929ff53213a1d2e581bedafb
DwGoing <jianghy1209@163.com>
2020-07-31 16:09:16 +0800

    初始化项目

---
+++ DwTransactionBus/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using DwFramework.Core.Plugins;
+
+namespace DwTransactionBus
+{
+    public class Transaction
+    {
+        public readonly string Id;
+        public readonly DateTime CreateTime;
+
+        public Transaction()
+        {
+            Id = Generater.GenerateGUID().ToString();
+            CreateTime = DateTime.Now;
+        }
+    }
+}
---
+++ DwTransactionBus/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using DwFramework.Core;
+using DwFramework.Core.Extensions;
+using DwFramework.WebAPI.Extensions;
+using DwFramework.RabbitMQ.Extensions;
+
+namespace DwTransactionBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var host = new ServiceHost(EnvironmentType.Develop, $"{AppDomain.CurrentDomain.BaseDirectory}");
+                host.RegisterLog();
+                host.RegisterWebAPIService();
+                host.InitService(provider => provider.InitWebAPIServiceAsync<Startup>());
+                host.RegisterRabbitMQService();
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"程序中断!:{ex.Message}");
+            }
+        }
+    }
+}
